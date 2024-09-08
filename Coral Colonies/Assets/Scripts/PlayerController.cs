13910dfcce51e05a5cd350c8f067be6eb283ce9f
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2024-09-07 23:07:55 -0500

    Created the player movement controls
    Player character now moves and it moves in the direction the player is looking at.

--- Coral Colonies/Assets/Scripts/PlayerController.cs
+++ Coral Colonies/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Reference the transform
+    Transform t;
+
+    [Header("Player Rotation")]
+    public float sensitivity = 1f;
+
+    //Mouse input variables
+    float rotationX;
+    float rotationY;
+
+    [Header("Player Movement")]
+    public float speed = 1f;
+    float moveX;
+    float moveY;
+    float moveZ;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        t = this.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
     void Update()
     {
-        
+        LookAround();
+
+        //Debug function to unlock cursor
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void LookAround()
+    {
+        //Get the mouse input
+        rotationX += Input.GetAxis("Mouse X") * sensitivity;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivity;
+
+        //Setting the rotation value every update
+        t.localRotation = Quaternion.Euler(-rotationY, rotationX, 0);
+
+    }
+
+    void Move()
+    {
+        //Get the movement input
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        moveZ = Input.GetAxis("Forward");
+
+        //Move the Character (Swimming ver)
+        t.Translate(new Vector3(moveX, 0, moveZ) * Time.deltaTime * speed);
+        t.Translate(new Vector3(0, moveY, 0) * Time.deltaTime * speed, Space.World);
     }
 }
